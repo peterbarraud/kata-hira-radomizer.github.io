4af54b9ab384668571b390d9405275a334dc4baa
Peter Barraud <gapeterb@gmail.com>
2025-11-10 11:11:11 +0530

    chekcing update

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 $(function() {
     console.log( "ready!" );
+    japaneseCharList = ['Vowels','K','S','T','N','H','M','Y','R','W','N'];
+    let currentIndex = japaneseCharList.length;
 
-    //credit: https://stackoverflow.com/a/2450976/4672179
-    let japaneseCharList = () => {
-        arrayToShuffle = ['Vowels','K','S','T','N','H','M','Y','R','W','N'];
-        let currentIndex = arrayToShuffle.length;
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
 
-        // While there remain elements to shuffle...
-        while (currentIndex != 0) {
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
 
-            // Pick a remaining element...
-            let randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex--;
+        // And swap it with the current element.
+        [japaneseCharList[currentIndex], japaneseCharList[randomIndex]] = [
+        japaneseCharList[randomIndex], japaneseCharList[currentIndex]];
 
-            // And swap it with the current element.
-            [arrayToShuffle[currentIndex], arrayToShuffle[randomIndex]] = [
-            arrayToShuffle[randomIndex], arrayToShuffle[currentIndex]];
-        }
-        return arrayToShuffle;
 
+        $('#japanese-chars').append(
+        $('<tr/>')
+            .addClass('char-style')
+            .append(`<td>${japaneseCharList[currentIndex]}</td>`)
+        );
+
+
+        console.log(japaneseCharList[currentIndex]);
     }
 
-    // Used like so
-    console.log(japaneseCharList());
-
-    
 });
